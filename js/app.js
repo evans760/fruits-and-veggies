1e2e83740b00b1f3031521071d27aa35066bb1d5
Thomas <evans_760@yahoo.com>
2016-07-22 09:03:04 -0700

    fruits and veggies

--- js/app.js
+++ js/app.js
@@ -4,3 +4,30 @@
 console.log('App Started');
 console.log('Fruit count', fruits.length);
 console.log('Veggie count', vegetables.length);
+
+
+var myApp = angular.module('FandV', []);
+
+myApp.controller('appCtrl', ['$scope', function($scope) {
+  $scope.fruits = [];
+  $scope.vegetables = [];
+  $scope.both = fruits.concat(vegetables);
+  $scope.click = function(destination, idx, currentArray)
+
+  {
+
+  $scope[destination].push($scope[currentArray] [idx]);
+  $scope[currentArray].splice(idx,1);
+}
+
+$scope.check = function(currItem, arrName) {
+  if(arrName === 'fruits') {
+    if(fruits.indexOf(currItem) === -1) {
+      return true;
+    }
+  } else {
+    if(vegetables.indexOf(currItem) === -1) {
+
+    }
+  }
+}}]);
